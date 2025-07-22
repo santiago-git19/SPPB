309074821304a62e7d76d74b04c13beec1f45d04
santiago-git19 <reinaalguacil@gmail.com>
2025-07-22 13:40:01 +0200

    new videos

--- Automatizacion/Examples/trt_pose_with_classification_example.py
+++ Automatizacion/Examples/trt_pose_with_classification_example.py
@@ -292,7 +292,7 @@ def main():
         'trt_pose_model': '/home/mobilenet/Documentos/Trabajo/trt_pose/models/resnet18_baseline_att_224x224_A_epoch_249.pth',
         'pose_topology': '/home/mobilenet/Documentos/Trabajo/trt_pose/tasks/human_pose/human_pose.json',
         'pose_classifier_model': '/home/mobilenet/Documentos/Trabajo/SPPB/models/pose_classification/st-gcn_3dbp_nvidia.engine',
-        'video_source': '/home/mobilenet/Documentos/Trabajo/SPPB/Automatizacion/sentado.mp4',  # Ruta para archivo de video
+        'video_source': '/home/mobilenet/Documentos/Trabajo/SPPB/Automatizacion/video_salida_1.mp4',  # Ruta para archivo de video
         'output_video': '/home/mobilenet/Documentos/Trabajo/SPPB/Automatizacion/output_with_poses.mp4'  # 'output_with_poses.mp4' para guardar
     }
     
