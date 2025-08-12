3b5395595819d7d3d420cb1f40ef347a0fe8f78e
santiagogit-19 <reinaalguacil@gmail.com>
2025-08-12 16:55:17 +0200

    m

--- Automatizacion/Examples/trt_pose_with_classification_example.py
+++ Automatizacion/Examples/trt_pose_with_classification_example.py
@@ -30,7 +30,7 @@ sys.path.append(str(Path(__file__).resolve().parent.parent))
 
 # Importar procesador existente de trt_pose y nuestro clasificador
 from utils.trt_pose_proc import TRTPoseProcessor
-from Automatizacion.utils.action_classifier import TRTPoseClassifier, create_pose_classifier
+from utils.action_classifier import TRTPoseClassifier, create_pose_classifier
 
 
 # Configurar logging
