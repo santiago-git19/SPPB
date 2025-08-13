f8f8c5f8076e19cb7bc5c5d9e5f4114bb7f0b25f
santiago-git19 <reinaalguacil@gmail.com>
2025-08-13 16:50:00 +0200

    m

--- Automatizacion/utils/action_classifier.py
+++ Automatizacion/utils/action_classifier.py
@@ -621,8 +621,9 @@ class TRTPoseClassifier:
             
             # Copiar resultado de GPU a CPU
             h_output = np.empty(self.output_shape, dtype=np.float32)
-            print(f"Salida cruda del modelo (h_output): {h_output}")
             cuda.memcpy_dtoh(h_output, self.d_output)
+            print(f"Salida cruda del modelo (h_output): {h_output}")
+
             
             inference_time = time.time() - start_time
             
