cb59be5fe1f79c9e5e0ed014e5a4cdcfa7e700ed
santiago-git19 <reinaalguacil@gmail.com>
2025-08-25 14:33:06 +0200

    m

--- Automatizacion/Examples/example_trt_pose.py
+++ Automatizacion/Examples/example_trt_pose.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+# Añadir el directorio 'Automatizacion' al sys.path
+sys.path.append(str(Path(__file__).resolve().parent.parent))
 from utils.trt_pose_proc import TRTPoseProcessor
 import cv2
 import time
